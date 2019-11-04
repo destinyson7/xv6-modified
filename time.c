4ae7ac338816a81c84755c17a9507732502cb050
destinyson7 <tanish.lad@research.iiit.ac.in>
2019-11-04 23:59:29 +0530

    base code + waitx implemented

---
+++ time.c
@@ -0,0 +1,37 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+int main(int argc, char *argv[])
+{
+  int pid = fork();
+
+  if(pid < 0)
+  {
+    printf(1, "Unable to fork\n");
+  }
+
+  else if(pid == 0)
+  {
+    for(int i = 0; i < argc-1; i++)
+    {
+      argv[i] = argv[i+1];
+    }
+    argv[argc-1] = 0;
+
+    exec(argv[0], argv);
+    printf(1, "exec: fail\n");
+  }
+
+  else
+  {
+    int wtime, rtime;
+    // printf(1, "******time.c  %d*******\n", pid);
+    waitx(&wtime, &rtime);
+
+    printf(1, "rtime = %d, wtime = %d\n", rtime, wtime);
+
+    exit();
+  }
+}
